88f9ed8d3791e443890015bac3a0cfc9c6e30dff
mohitkr07 <2020kuec2031@iiitkota.ac.in>
2023-07-13 20:36:07 +0530

    on production

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,22 @@ const cors = require("cors");
 const axios = require("axios");
 const _ = require("lodash");
 const app = express();
-const port = process.env.PORT || 5000;
+const path = require("path");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
+app.use(express.static("public"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
 
 const months = [
   "January",
@@ -146,6 +157,10 @@ app.post("/", async (req, res) => {
       res.status(404).send({ message: "something went wrong" });
     }
   });
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
